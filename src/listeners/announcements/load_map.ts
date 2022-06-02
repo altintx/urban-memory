4a2b1f8593d79b615ffcab8853cfef73e4ffe1aa
Brian Bosh <bbosh@alternateinterior.com>
2022-06-02 17:56:20 -0600

    "walls" idea

---
+++ src/listeners/announcements/load_map.ts
@@ -0,0 +1,9 @@
+import { Operator } from "../../models/core/operator";
+import { Game } from "../../models/game";
+import { Map } from "../../models/map/map";
+
+export default function loadMapAnnouncement(game: Game, map: Map, operator: Operator) {
+    game.operators.filter(o => o !== operator).map(existingOperator => {
+        existingOperator.socket.emit("load_map", map);
+    });
+}
--- src/models/map/tile.ts
+++ src/models/map/tile.ts
@@ -1,15 +1,27 @@
 import { Character } from "../characters/character";
 import { Obstacle } from "../obstacle/obstacle";
 
+enum Cover { None, Half, Full };
+enum WallType { Fence, Wall };
+
+type Wall = {
+    cover: Cover;
+    type: WallType;
+    destructable: boolean;
+}
+
 type Tile = {
     elevation: number;
     occupant?: Character | Obstacle;
     destructive: boolean;
     destructable: boolean;
     visible: boolean;
-    
+    wall: Wall[];
 }
 
 export {
-    Tile
+    Tile,
+    Cover,
+    WallType,
+    Wall
 };
