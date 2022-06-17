f672291ec31cb3b6b5912a875d471af626397a59
Brian Bosh <bbosh@alternateinterior.com>
2022-06-16 21:38:16 -0600

    tests for map-related functions

---
+++ src/models/map/map.spec.ts
@@ -0,0 +1,29 @@
+import { randomUUID } from 'crypto';
+import {OutOfBoundsError, at} from './map'; 
+
+const oneByOneMap = { grid: [{ elevation: 0, cover: null, openable: false } ], width: 1, height: 1, uuid: randomUUID() };
+
+describe('OutOfBoundsError', () => {
+    it("Has expected properties", () => {
+        const g = new OutOfBoundsError(0,0,0,0);
+        expect(g.message).toBe(`Valid coordinates are (0 <= x < 0, 0 <= y < 0)`);
+        expect(g.x).toBe(0);
+        expect(g.y).toBe(0);
+    })
+});
+
+describe("at", () => {
+    it("Should retrieve a tile without boundaries", () => {
+        const tile = at(oneByOneMap, 0, 0)
+        expect(tile).toBe(oneByOneMap.grid[0]);
+    });
+    it("Should error if requested tile is out of bounds", () => {
+        let success = false;
+        try {
+            at(oneByOneMap, 0, 1);
+        } catch (e) {
+            success = true; // there's got to be a better way to do this
+        }
+        expect(success).toBeTruthy();
+    })
+})
