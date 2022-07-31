bf578ba092c6700c3c88b869904b216a56042d81
Brian Bosh <bbosh@alternateinterior.com>
2022-07-31 13:18:48 -0600

    capture requested language at initial connection

--- src/websocket/listeners/announcements/hello.ts
+++ src/websocket/listeners/announcements/hello.ts
@@ -1,5 +1,7 @@
 import { Socket } from "socket.io";
+import {parse} from 'accept-language-parser';
 
 export default function helloAnnouncement(socket: Socket) {
-    socket.emit("hello");
+    const language = parse(socket.handshake.headers['accept-language']);
+    socket.emit("hello", { language: `${language[0].code}-${language[0].region}` });
 }
