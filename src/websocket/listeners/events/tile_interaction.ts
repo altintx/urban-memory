347e3bd4a81702bb62990daed607b8ee64eff45c
Brian Bosh <bbosh@alternateinterior.com>
2022-08-08 21:41:45 -0600

    signature tracking on selection events

--- src/websocket/listeners/events/tile_interaction.ts
+++ src/websocket/listeners/events/tile_interaction.ts
@@ -1,15 +1,14 @@
 import { Socket } from "socket.io";
 import { getOperator, setGame } from "../../../sessions";
 import { at } from "../../../models/map/map";
-import missionInfoAnnouncement from "../announcements/mission_info";
 import styleTileAnnouncement from "../announcements/style_tile";
 
 export enum Modes { Hovering, Selected, Passive, Clear, Point }
-export function tileInteraction(socket: Socket, { x, y, mode, }: { x: number, y: number, mode: string }) {
+export function tileInteraction(socket: Socket, { x, y, mode, sig, }: { x: number, y: number, mode: string, sig: string }) {
     const operator = getOperator(socket);
     const game = operator.game;
     const tile = at(game.activeMission.map, x, y);
     game.operators.forEach(o => {
-        styleTileAnnouncement(o, tile, operator, mode);
+        styleTileAnnouncement(o, tile, operator, mode, sig);
     });
 }
