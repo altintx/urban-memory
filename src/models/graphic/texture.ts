e88d9fa4dc30690f53419df51a81d9f9552356bc
Brian Bosh <bbosh@alternateinterior.com>
2022-08-21 18:01:47 -0600

    animation

--- src/models/graphic/texture.ts
+++ src/models/graphic/texture.ts
@@ -1,15 +1,29 @@
 import { Offset, parseOffset } from "./offset";
 
+export type AnimationCell = {
+    graphic: string,
+    direction: string;
+    ms: number;
+}
+
 export type Texture = {
     graphic: string;
-    animation: any[];
+    animation: AnimationCell[];
     offset: Offset;
+}
+
+export function parseAnimationCell(json: any): any {
+    return {
+        graphic: json['graphic'],
+        direction: json['direction'],
+        ms: parseInt(json['ms'])
+    };
 }
 
 export function parseTexture(json): Texture {
     return {
         graphic: json['graphic'],
-        animation: [],
+        animation: json['animation'].map(json => parseAnimationCell(json)),
         offset: parseOffset(json['offset'])
     }
 }
